9fa7ccc7ee193b80f5c968958f136a03ed80d34f
Nithish Karthik <nithishkarthik01@gmail.com>
2023-12-30 09:44:14 +0530

    refactor parser

--- parser.c
+++ parser.c
@@ -30,56 +30,54 @@ int parse_expression(StringBuilder* sb, int* lexer_idx, Expression* expr){
 
 int parse_statement(StringBuilder* sb, int* lexer_idx, ReturnStatement* st){
 	Token tkn ={0};
+
 	*lexer_idx += find_next_token(sb->items+(*lexer_idx), &tkn);
-	if(tkn.type == Keyword && !strcmp(tkn.value, "return")){
-		Expression* expr = malloc(sizeof(Expression));
-		assert(expr != NULL);
-		if(parse_expression(sb, lexer_idx, expr)){
-			st->expr = expr;
-			*lexer_idx += find_next_token(sb->items+(*lexer_idx), &tkn);
-			if(tkn.type == SEMICOLON){
-				return PARSE_SUCCESS;
-			} else return PARSE_FAIL;
-		}else return PARSE_FAIL;
-	} else return PARSE_FAIL;
+	if(!(tkn.type == Keyword && !strcmp(tkn.value, "return"))) return PARSE_FAIL;
+
+    Expression* expr = malloc(sizeof(Expression));
+    assert(expr != NULL);
+    if(!parse_expression(sb, lexer_idx, expr))return PARSE_FAIL;
+    st->expr = expr;
+
+    *lexer_idx += find_next_token(sb->items+(*lexer_idx), &tkn);
+    if(tkn.type != SEMICOLON) return PARSE_FAIL;
+    return PARSE_SUCCESS;
 }
 
 int parse_function(StringBuilder* sb, int* lexer_idx, Function* fn){
     Token tkn ={0};
     *lexer_idx += find_next_token(sb->items+(*lexer_idx), &tkn);
-    if(tkn.type == Keyword && !strcmp(tkn.value, "int")){
-	    *lexer_idx += find_next_token(sb->items+(*lexer_idx), &tkn);
-   	     if(tkn.type == Id){
-		fn->name = tkn.value;
-		*lexer_idx += find_next_token(sb->items+(*lexer_idx), &tkn);
-		if(tkn.type == OPEN_PARANTHESIS){
-			*lexer_idx += find_next_token(sb->items+(*lexer_idx), &tkn);
-			if(tkn.type == CLOSE_PARANTHESIS){
-				*lexer_idx += find_next_token(sb->items+(*lexer_idx), &tkn);
-				if(tkn.type == OPEN_BRACE){
-					ReturnStatement* st = malloc(sizeof(ReturnStatement));
-					assert(st != NULL);
-					if(parse_statement(sb, lexer_idx, st)){
-						fn->st = st;
-						*lexer_idx += find_next_token(sb->items+(*lexer_idx), &tkn);
-						if(tkn.type == CLOSE_BRACE){
-								return PARSE_SUCCESS;
-						} else return PARSE_FAIL;
-					}else return PARSE_FAIL;
-				} else return PARSE_FAIL;
-			} else return PARSE_FAIL;
-		} else return PARSE_FAIL;
-	    } else return PARSE_FAIL;
-    }else return PARSE_FAIL;
+    if(!(tkn.type == Keyword && !strcmp(tkn.value, "int"))) return PARSE_FAIL;
+
+    *lexer_idx += find_next_token(sb->items+(*lexer_idx), &tkn);
+    if(tkn.type != Id) return PARSE_FAIL;
+    fn->name = tkn.value;
+
+    *lexer_idx += find_next_token(sb->items+(*lexer_idx), &tkn);
+    if(tkn.type != OPEN_PARANTHESIS) return PARSE_FAIL;
+
+    *lexer_idx += find_next_token(sb->items+(*lexer_idx), &tkn);
+    if(tkn.type != CLOSE_PARANTHESIS) return PARSE_FAIL;
+
+    *lexer_idx += find_next_token(sb->items+(*lexer_idx), &tkn);
+    if(tkn.type != OPEN_BRACE) return PARSE_FAIL;
+
+    ReturnStatement* st = malloc(sizeof(ReturnStatement));
+    assert(st != NULL);
+    if(!parse_statement(sb, lexer_idx, st)) return PARSE_FAIL;
+    fn->st = st;
+
+    *lexer_idx += find_next_token(sb->items+(*lexer_idx), &tkn);
+    if(tkn.type != CLOSE_BRACE) return PARSE_FAIL;
+    return PARSE_SUCCESS;
 }
 
 int parse_program(StringBuilder* sb, int* lexer_idx, Program* pr){
 	Function* fn = malloc(sizeof(Function));
 	assert(fn != NULL);
-	if(parse_function(sb, lexer_idx, fn)){
-		pr->fn = fn;
-		return PARSE_SUCCESS;
-	} else return PARSE_FAIL;
+	if(!parse_function(sb, lexer_idx, fn)) return PARSE_FAIL;
+    pr->fn = fn;
+    return PARSE_SUCCESS;
 }
 
 void free_statement(ReturnStatement* st){
@@ -95,6 +93,7 @@ void free_program(Program* pr){
 	free_function(pr->fn);
 }
 
+// Make sure to free the Program 
 int parse_file(FILE* fp, Program* pr){
     StringBuilder sb = {0};
     read_source_file(fp, &sb);
